6994052d538eae88a3baaebe6c51d341c80de54d
Ewelina Szoda <ewelinaszoda@gmail.com>
2020-04-09 12:40:11 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
 # Add your variables here
-first_number = "number"
-second_number != 0
+first_number = 1
+second_number = 2
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
